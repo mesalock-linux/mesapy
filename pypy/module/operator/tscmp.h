a93db66d12571a3ed0af0541a35741635bee0688
Melody <hxw142830@utdallas.edu>
2018-07-02 16:52:49 -0700

    update the directory of wchar.h

--- pypy/module/operator/tscmp.h
+++ pypy/module/operator/tscmp.h
@@ -1,3 +1,3 @@
-#include "/opt/intel/sgxsdk/include/tlibc/wchar.h"
+#include <wchar.h>
 RPY_EXTERN int pypy_tscmp(const char *, const char *, long, long);
 RPY_EXTERN int pypy_tscmp_wide(const wchar_t *, const wchar_t *, long, long);
