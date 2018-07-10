8dec67317be8751bfe97253427e333823d8fd110
Melody <hxw142830@utdallas.edu>
2018-07-09 19:32:33 -0700

    move ocall_printf to example

--- sgx/Sgx_App_Enclave/Enclave/Enclave_syscalls.c
+++ sgx/Sgx_App_Enclave/Enclave/Enclave_syscalls.c
@@ -22,17 +22,6 @@ unsigned long write(long fd, long buf, long count){
         return ret;
 }
 
-void ocall_printf(const char *fmt, ...)
-{
-    char buf[BUFSIZ] = {'\0'};
-    va_list ap;
-    va_start(ap, fmt);
-    vsnprintf(buf, BUFSIZ, fmt, ap);
-    va_end(ap);
-    ocall_print_string(buf);
-}
-
-
 #if defined(__cplusplus)
 }
 #endif
