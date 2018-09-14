484b56733e7f88df2a38c42c697d06a94aebed2b
MelodyHuibo <hxw142830@utdallas.edu>
2018-09-13 19:20:21 -0700

    typo

--- sgx/Sgx_App_Enclave/Enclave/Enclave_syscalls.c
+++ sgx/Sgx_App_Enclave/Enclave/Enclave_syscalls.c
@@ -13,7 +13,7 @@ extern void* sgx_ocalloc(unsigned long size);
 extern void sgx_ocfree(void);
 
 ssize_t write(int fd, const void *buf, size_t count){
-        ssize_t reti = -1;
+        ssize_t ret = -1;
         ocall_write(&ret, fd, buf, count);	
         return ret;
 }
