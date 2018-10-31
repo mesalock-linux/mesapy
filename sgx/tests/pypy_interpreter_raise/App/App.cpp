41e8ba94bdaed03ab037c11063e7c772644288d0
Mingshen Sun <mssun@mesalock-linux.org>
2018-10-31 10:21:18 -0700

    pypy/tests: fix typo

--- sgx/tests/pypy_interpreter_raise/App/App.cpp
+++ sgx/tests/pypy_interpreter_raise/App/App.cpp
@@ -156,7 +156,7 @@ int SGX_CDECL main(int argc, char *argv[])
     /* Destroy the enclave */
     sgx_destroy_enclave(global_eid);
 
-    printf("[+]: test_raisesuccessfully returned\n");
+    printf("[+]: test_raise successfully returned\n");
 
     return 0;
 }
